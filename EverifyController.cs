ff71fbb481c33e99bbd957e5168512a8a06861d6
bstoinev <matthew@mail.bg>
2018-09-26 10:35:38 +0300

    Samples commit

---
+++ EverifyController.cs
@@ -0,0 +1,269 @@
+﻿using LookoutService.EVerify.Data.Request;
+using LookoutService.EVerify.Data.Response;
+using LookoutService.Model.BusinessInteligence;
+using LookoutService.Model.BusinessInteligence.Everify;
+using LookoutService.Model.Dto.Everify;
+using LookoutService.Web.Models.Everify;
+using LookoutService.Web.Services.EVerifyManagement.ServiceContracts;
+using LookoutService.Web.Services.IcaManagement;
+using LookoutService.Web.ViewModel.Infrastructure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LookoutService.Web.Controllers
+{
+    public class EverifyController : BaseController
+    {
+        private readonly IEVerifyManagementService evManagement = null;
+        private readonly IIcaDataManagementService icaManagement = null;
+
+        public EverifyController(IEVerifyManagementService everifyManagementService, IIcaDataManagementService icaDataManagementService)
+        {
+            evManagement = everifyManagementService;
+            icaManagement = icaDataManagementService;
+        }
+
+        public JsonResult TncDropContest(EverifyCaseDto EverifyCase)
+        {
+            var result = new JsonResultModel<EverifyCaseJobDto>();
+
+            try
+            {
+                result.Model = evManagement.TncDropContest(EverifyCase, SiteContext.User.UserId);
+                result.Message = "Contest disclaimer recorded.";
+            }
+            catch (Exception ex)
+            {
+                result.HasError = true;
+                result.Message = string.Concat("Error occurred: ", ex.Message);
+                Logger.Error("TncDropContest failed with the following exception:", ex);
+            }
+            
+            return Json(result);
+        }
+
+        public ActionResult CaseProcessingIneligible()
+        {
+            return PartialView("~/Views/Everify/_CaseProcessing-Ineligible.cshtml");
+        }
+
+        public ActionResult CaseProcessingPending()
+        {
+            return PartialView("~/Views/Everify/_CaseProcessing-Pending.cshtml");
+        }
+
+        public ActionResult CaseProcessingUnqualified()
+        {
+            return PartialView("~/Views/Everify/_CaseProcessing-Unqualified.cshtml");
+        }
+
+        public ActionResult QueryCaseHistory(EverifyCaseDto dto)
+        {
+            if (dto == null || !dto.Id.HasValue)
+            {
+                throw new ArgumentException("Case could not be determined.");
+            }
+
+            var criterionDto = new EverifyCaseJobDto
+            {
+                Case = new EverifyCaseDto { Id = dto.Id }
+            };
+
+            var jobs = evManagement.LoadJobs(criterionDto);
+
+            var vm = new EverifyCaseHistoryModel
+            {
+                Events = jobs.Select(j => EverifyCaseHistoryRecord.FromJob(j))
+            };
+
+            return PartialView("~/Views/Everify/_CaseHistory.cshtml", vm);
+        }
+
+        public ActionResult QueryCaseProcessing(EverifyCaseDto dto)
+        {
+            var existingCases = evManagement.LoadEverifyCases(dto);
+            var activeCase = (IEverifyCase)existingCases.FirstOrDefault(ec => !ec.State.Value.HasFlag(EverifyCaseState.Closed));
+            
+            var viewName = string.Empty;
+
+            if (activeCase == null)
+            {
+                throw new ArgumentException("Invalid E-Verify case specified.");
+            }
+
+            var mrj = activeCase.Jobs.LastOrDefault();
+            var currentState = activeCase.State;
+
+            if (currentState == EverifyCaseState.Pending)
+            {
+                viewName = "_CaseProcessing-Pending.cshtml";
+            }
+            else if (currentState.HasFlag(EverifyCaseState.SsaTnc))
+            {
+                var processState = TncState.Unknown;
+                var lastTncJob = mrj;
+
+                if (lastTncJob == null)
+                {
+
+                }
+                else if (lastTncJob.JobType == EverifyCaseJobType.SsaReferral)
+                {
+                    var referralRes = JsonConvert.DeserializeObject<SsaReferralResponse>(lastTncJob.Response);
+
+                    processState |= referralRes != null && referralRes.StatusCode == 0 ? TncState.ReferralInitiated : TncState.Error;
+                }
+                else if (lastTncJob.JobType == EverifyCaseJobType.SsaNotify)
+                {
+                    var notificationReq = JsonConvert.DeserializeObject<TncNotificationRequest>(lastTncJob.Request);
+                    var notificationRes = JsonConvert.DeserializeObject<TncNotificationResponse>(lastTncJob.Response);
+
+                    processState |= notificationReq.EmployeeNotified ? TncState.NotificationSucceeded : TncState.NotificationFailed;
+
+                    if (notificationRes == null || notificationRes.StatusCode != 0)
+                    {
+                        processState |= TncState.Error;
+                    }
+                }
+
+                ViewBag.TncProcessState = processState;
+                viewName = "_CaseProcessing-TNC.cshtml";
+            }
+            else if (currentState.HasFlag(EverifyCaseState.InitialVerification))
+            {
+                var verificationJob = activeCase.Jobs.LastOrDefault(j => j.JobType == EverifyCaseJobType.InitialVerification);
+                var verificationRes = JsonConvert.DeserializeObject<VerificationResponse>(verificationJob.Response ?? string.Empty);
+
+                if (currentState.HasFlag(EverifyCaseState.Error))
+                {
+                    viewName = "_CaseProcessing-VerificationFailed.cshtml";
+
+                    if (verificationRes == null)
+                    {
+                        ViewBag.ProcessingAssistanceHtml = verificationJob.Response;
+                    }
+                    else
+                    {
+                        ViewBag.ProcessingAssistanceHtml = string.Format("Error code: {0}<br />Error message: {1}", verificationRes.StatusCode, verificationRes.StatusMessage);
+                    }
+                }
+                else if (currentState.HasFlag(EverifyCaseState.EmploymentAuthorized))
+                {
+                    viewName = "_CaseProcessing-EmploymentAuthorized.cshtml";
+                }
+                else if (currentState.HasFlag(EverifyCaseState.SsaCaseIncomplete))
+                {
+                    ViewBag.EligibilityStatementMessage = verificationRes.EligibilityStatementMessage;
+                    ViewBag.EligibilityStatementDescription = verificationRes.EligibilityStatementDescription;
+
+                    viewName = "_CaseProcessing-CaseIncomplete.cshtml";
+                }
+                else if (currentState == EverifyCaseState.PhotoConfirmationRequired)
+                {
+                    viewName = "_CaseProcessing-PhotoMatch.cshtml";
+                }
+                else if (currentState.HasFlag(EverifyCaseState.SsaTnc) || currentState.HasFlag(EverifyCaseState.DhsTnc))
+                {
+                    viewName = "_CaseProcessing-TNC.cshtml";
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Unknown E-Verify case state: {0}", currentState.ToString());
+            }
+            
+            return PartialView(string.Concat("~/Views/Everify/", viewName));
+        }
+
+        [HttpPost]
+        public ActionResult SsaReverify(EverifyCaseDto EverifyCase)
+        {
+            var result = new JsonResultModel<EverifyCaseJobDto>();
+
+            try
+            {
+                result.Model = evManagement.TriggerSsaReverification(EverifyCase, SiteContext.User.UserId);
+            }
+            catch (Exception ex)
+            {
+                result.HasError = true;
+                result.Message = string.Concat("Error occurred: ", ex.Message);
+                Logger.Error("SsaReverify failed with the following exception:", ex);
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult SsaTncEmployeeNotificationFailed(EverifyCaseDto EverifyCase)
+        {
+            var result = new JsonResultModel<EverifyCaseJobDto>();
+
+            try
+            {
+                result.Model = evManagement.SsaTncEmployeeNotificationFailed(EverifyCase, SiteContext.User.UserId);
+            }
+            catch (Exception ex)
+            {
+                result.HasError = true;
+                result.Message = string.Concat("Error occurred: ", ex.Message);
+                Logger.Error("SsaNotificationFailed failed with the following exception:", ex);
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult SsaTncEmployeeNotificationSucceeded(EverifyCaseDto EverifyCase)
+        {
+            var result = new JsonResultModel<EverifyCaseJobDto>();
+
+            try
+            {
+                result.Model = evManagement.SsaTncEmployeeNotificationSucceeded(EverifyCase, SiteContext.User.UserId);
+            }
+            catch (Exception ex)
+            {
+                result.HasError = true;
+                result.Message = string.Concat("Error occurred: ", ex.Message);
+                Logger.Error("SsatncEmployeeNotificationSucceeded failed with the following exception:", ex);
+            }
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public JsonResult TriggerSsaReferral(EverifyCaseDto EverifyCase)
+        {
+            var result = new JsonResultModel<EverifyCaseJobDto>();
+
+            try
+            {
+                result.Model = evManagement.TriggerSsaReferral(EverifyCase, SiteContext.User.UserId);
+                result.Message = "Referral initiated.";
+            }
+            catch (Exception ex)
+            {
+                result.HasError = true;
+                result.Message = string.Concat("Error occurred: ", ex.Message);
+                Logger.Error("TriggerSsaReferral failed with the following exception:", ex);
+            }
+
+            return Json(result);
+        }
+
+        
+
+        [HttpPost]
+        public ActionResult TriggerVerification(EverifyCaseDto EverifyCase)
+        {
+            evManagement.TriggerVerification(EverifyCase, SiteContext.User.UserId);
+
+            return RedirectToAction("QueryCaseProcessing", EverifyCase);
+        }
+    }
+}
---
+++ NonFederalWorkingDayController.cs
@@ -0,0 +1,103 @@
+﻿using LookoutService.Model.Dto;
+using LookoutService.Web.Services.NonFederalWorkingDayManagement;
+using LookoutService.Web.ViewModel.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace LookoutService.Web.Controllers
+{
+    public class NonFederalWorkingDayController : BaseController
+    {
+        private readonly INonFederalWorkingDayManagementService _nonFederalWorkingDayService = null;
+
+        public NonFederalWorkingDayController(INonFederalWorkingDayManagementService nonFederalWorkingDayService)
+        {
+            _nonFederalWorkingDayService = nonFederalWorkingDayService;
+        }
+
+        private JsonResultModel<IEnumerable<NonFederalWorkingDayDto>> InvokeService(Func<IEnumerable<NonFederalWorkingDayDto>, I9UserDto, IEnumerable<NonFederalWorkingDayDto>> operation, IEnumerable<NonFederalWorkingDayDto> models)
+        {
+            var result = new JsonResultModel<IEnumerable<NonFederalWorkingDayDto>>();
+
+            if (ModelState.IsValid)
+            {
+                var modifier = new I9UserDto
+                {
+                    Id = SiteContext.User.UserId
+                };
+
+                try
+                {
+                    result.Model = operation(models, modifier);
+                    result.Message = "Operation completed successfully.";
+                }
+                catch (Exception ex)
+                {
+                    result.HasError = true;
+#if DEBUG
+                    result.Message = string.Concat("Operation failed: ", ex.Message);
+#else
+                    result.Message = "Operation failed: Internal server error.";
+#endif
+                }
+
+                if (result.Model != null)
+                {
+                    foreach (var dto in result.Model)
+                    {
+                        dto.CreatedOn = null;
+                        dto.Creator = null;
+                        dto.ModifiedOn = null;
+                        dto.Modifier = null;
+                    }
+                }
+            }
+            else
+            {
+                result.HasError = true;
+                result.Message = "Invalid data.";
+            }
+
+            return result;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _nonFederalWorkingDayService.Load();
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IEnumerable<NonFederalWorkingDayDto> models)
+        {
+            var result = InvokeService(_nonFederalWorkingDayService.Create, models);
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult Update(IEnumerable<NonFederalWorkingDayDto> models)
+        {
+            var result = InvokeService(_nonFederalWorkingDayService.Update, models);
+
+            return Json(result);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(IEnumerable<NonFederalWorkingDayDto> models)
+        {
+            var result = InvokeService(_nonFederalWorkingDayService.Delete, models);
+
+            if (!result.HasError)
+            {
+                var plurality = models.Count() == 1 ? "Entity" : "Entities";
+                result.Message = string.Concat(plurality, " deleted successfully.");
+            }
+
+            return Json(result);
+        }
+    }
+}
